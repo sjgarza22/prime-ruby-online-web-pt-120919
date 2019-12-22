4f8adbc24b11866c0dbe1c7f973262d7bf93c713
Sara Garza <sjgarza22@gmail.com>
2019-12-22 20:48:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(num)
-  factors = [1]
-  i = 2
   if num > 1
+    factors = [1]
+    numbers = (1..num).to_a
     while factors.length < 3 && i <= num
       if num % i == 0
         factors << i
