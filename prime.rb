858a841a811dda52fc28d1191cd4b0af1a723ab3
Sara Garza <sjgarza22@gmail.com>
2019-12-22 20:58:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num)
+  if num > 1
+    factors = []
+    numbers = (1..num).to_a
+    numbers.each do |number|
+      if num % number == 0
+        factors << number
+      end
+    end
+  else
+    return false
+  end
+
+
+  if factors.length < 3
+    return true
+  else
+    return false
+  end
+end
