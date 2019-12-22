af0e23d065e0447787af2252ad8bde12ddc4ed01
Sara Garza <sjgarza22@gmail.com>
2019-12-22 20:52:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,9 @@
 # Add  code here!
 def prime?(num)
   if num > 1
-    factors = [1]
+    factors = []
     numbers = (1..num).to_a
-    while factors.length < 3 && i <= num
-      if num % i == 0
-        factors << i
-      end
-      i += 1
-    end
-  else
-    return false
-  end
+    
 
   if factors.length < 3
     return true
