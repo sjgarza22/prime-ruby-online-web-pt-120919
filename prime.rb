5d2ed8470e1302165f6010d73a5102e4ea204643
Sara Garza <sjgarza22@gmail.com>
2019-12-22 20:14:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,12 @@ def prime?(num)
   factors = [1..num]
   array = []
   factors.each do |factor|
-
+    if num % factor == 0 && array.length < 3
+      array << factor
+    elsif array.length > 2
+      return false
+    else
+      return true
+    end
+  end
 end
