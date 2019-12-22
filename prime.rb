e8612fa935ce83069e2e75e38d15c05411b47763
Sara Garza <sjgarza22@gmail.com>
2019-12-22 20:16:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(num)
-  factors = [1..num]
+  possible_factors = [1..num]
   array = [1, num]
-  factors.each do |factor|
+  possible_factors.each do |factor|
     if num % factor == 0 && array.length < 3
       array << factor
     elsif array.length > 2
