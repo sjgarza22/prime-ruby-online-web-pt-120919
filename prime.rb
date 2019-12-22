890fa2dcaec713466870cfc42199ea300b75e06e
Sara Garza <sjgarza22@gmail.com>
2019-12-22 20:56:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,10 @@ def prime?(num)
   if num > 1
     factors = []
     numbers = (1..num).to_a
+    numbers.each do |number|
+      if num % number == 0
+        factors << number
+      end
 
 
   if factors.length < 3
